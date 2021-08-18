23f3e102519ae48cdb6f6b3f6b9ecb804fb0b1cf
fayazsin <Fatih1453?>
2021-08-18 11:32:08 +0200

    udt1

--- src/UseEffectFetch.js
+++ src/UseEffectFetch.js
@@ -9,14 +9,15 @@ function UseEffectFetch() {
     }, [])
     //console.log(ulkeler)
     return (
-        <div className="d-flex text-center mt-5">
+        <div className="text-center mt-5">
+            <h1 className="bg-warning"> COUNTRY INFORMATION</h1>
 
             {
 
                 ulkeler.map((ulke) => {
                     const { id, name, capital, flag } = ulke
                     return (
-                        <div key={id}>
+                        <div key={name}>
                             <img src={flag} alt="" width="50%" />
                             <div className="" >
                                 <h4>{name}</h4>
